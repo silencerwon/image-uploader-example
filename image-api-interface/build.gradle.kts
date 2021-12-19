d0aa98579ab28bfce4a40ce93beb94eac056fb10
silencer <silencer@woowahan.com>
2021-12-19 17:09:10 +0900

    chore: build.gradle.kts 수정
    - plain jar 생성되지 않도록 코드 수정

--- image-api-interface/build.gradle.kts
+++ image-api-interface/build.gradle.kts
@@ -16,3 +16,9 @@ dependencies {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
 }
+
+val jar: Jar by tasks
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
+
+bootJar.enabled = true
+jar.enabled = false
